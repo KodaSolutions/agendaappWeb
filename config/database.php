46ee6a040f8fe1715ee0f4e36209d04e8c1ca8f1
aalcolea <46306963+aalcolea@users.noreply.github.com>
2024-04-24 00:30:58 -0600

    database configuration

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL=parse_url(‘DATABASE_URL’);
 
 return [
 
@@ -46,10 +47,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],,
+            'database' => ltrim($DATABASE_URL['path'], "/"),
+            'username' => $DATABASE_URL['pass'],
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
