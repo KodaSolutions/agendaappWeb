82ae545922d0b6649f8411fced404559d629baf7
aalcolea <46306963+aalcolea@users.noreply.github.com>
2024-04-25 01:01:58 -0600

    database changes

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -13,7 +13,20 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-
+        $user1 = User::create([
+            'name' => 'Doctor Uno',
+            'email' => 'doctor1@test.com',
+            'password' => Hash::make('123456'),
+        ]);
+        UserDetail::create([
+            'user_id' => $user1->id,
+            'role_id' => 1,
+            'name' => 'test',
+            'age' => 1,
+            'phone' => '123456789',
+            'gender' => 'M',
+            'birthday' => '2024-04-04',
+        ]);
         $user2 = User::create([
             'name' => 'Doctor Dos',
             'email' => 'doctor2@test.com',
