d6fc43830814fba413ed4e59846339b05a05f70d
aalcolea <46306963+aalcolea@users.noreply.github.com>
2024-05-09 05:39:49 -0600

    modifying store appoinment funciton

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -6,24 +6,30 @@ use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
 {
-    public function store(Request $request){
-        $validatedData = $request->validate([
-            'client_id' => 'required|exists:clients,id',
-            'appointment_date' => 'required|date',
-            'treatment_type' => 'required|string'
-        ]);
-        $user = Auth::user();
+public function store(Request $request){
+    $validatedData = $request->validate([
+        'client_id' => 'required|exists:clients,id',
+        'date' => 'required|date',
+        'time' => 'required', 
+        'treatment' => 'required|string', 
+    ]);
+    
+    $user = Auth::user();
 
-        $appointment = new Appointment;
-        $appointment->client_id = $validatedData['client_id'];
-        $appointment->created_by = $user->id;
-        $appointment->doctor_id = $user->id;
-        $appointment->appointment_date = $validatedData['appointment_date'];
-        $appointment->treatment_type = $validatedData['treatment_type'];
-        $appointment->status = $validatedData['status'];
-        
-        $appointment->save();
+    $appointment = new Appointment;
+    $appointment->client_id = $validatedData['client_id'];
+    $appointment->created_by = $user->id;
+    $appointment->doctor_id = $user->id; 
+    $appointment->appointment_date = $validatedData['date'] . ' ' . $validatedData['time']; 
+    $appointment->treatment_type = $validatedData['treatment'];
+    $appointment->status = 'Upcoming'; 
 
-        return response()->json(['message' => 'Appointment creado correctamente', 'appointment' => $appointment], 201);
-    }
+    $appointment->save(); 
+
+    return response()->json([
+        'message' => 'Appointment creado correctamente',
+        'appointment' => $appointment
+    ], 201);
+}
+
 }
