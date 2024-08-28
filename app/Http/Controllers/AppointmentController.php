845618435033afbb1f1f2d60531b9797157de404
alanalcolea <alanalcolea@gmail.com>
2024-08-28 04:11:26 -0700

    update

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Appointment;
+use App\Models\User;
 use Auth;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Notification;
