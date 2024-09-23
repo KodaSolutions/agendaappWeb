d9d6a5605d170f5b2f982661119d9353af619dfc
aalcolea <alanalcolea@gmail.com>
2024-09-23 00:26:51 -0600

    adding client 0

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,39 @@ Route::get('/', function () {
 });
 Route::get('userAll', [AuthController::class, 'userAll']);
 
+use App\Models\Client;
+use App\Models\UserDetail;
+
+Route::get('/insert-client', function () {
+    // Intentar obtener el cliente con ID 1
+    $client = Client::find(1);
+
+    // Si no existe, crear uno nuevo con ID 1
+    if (!$client) {
+        $client = new Client([
+            'id' => 1, // Forzar el ID
+            'name' => 'Cliente ',
+            'number' => '0000000000',
+            'email' => 'cliente@cliente.com',
+            'visit_count' => 0,
+        ]);
+        $client->save();
+    }
+
+    // Insertar en la tabla user_details
+    $userDetail = UserDetail::create([
+        'name' => 'Nombre por defecto',
+        'role_id' => 1, // Ajusta este valor según sea necesario
+        'user_id' => $client->id, // Usar el ID del cliente
+        'age' => 0, // Ajusta según sea necesario
+        'phone' => '000-000-0000', // Valor por defecto
+        'gender' => 'no especificado', // Valor por defecto
+    ]);
+
+    return response()->json([
+        'client' => $client,
+        'userDetail' => $userDetail,
+    ]);
+});
+
+
