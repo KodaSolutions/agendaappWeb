5de6e2af7f8eb066b43da2d672d76f8807905795
aalcolea <alanalcolea@gmail.com>
2024-09-23 00:57:30 -0600

    cannot delete cliente 0

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -32,25 +32,30 @@ class ClientController extends Controller
         
         return response()->json(['message' => 'Client creado correctamente', 'client' => $client], 201);
     }
-    public function deleteClient($id){
-        try {
-            $client = Client::find($id);
-            if($id === 1){
-                return response()->json(['message' => 'Este cliente es generico y no puede eliminarse'], 302);
+public function deleteClient($id)
+{
+    try {
+        // Verifica si el cliente existe
+        $client = Client::find($id);
 
-            }else{
-                if($client){
-                    if($client->delete()){
-                        return response()->json(['message' => 'Cliente eliminado con exito', 'client' => $client], 200);
-                    }
-                }else{
-                    return response()->json(['message' => 'Cliente no encontrado'], 404);
-                }
-            }
-        } catch (Exception $e) {
-            return response()->json(['message'=> 'Error al eliminar cliente', 'error' => $e->getMessage()], 500);
+        // Validar que el cliente no sea el cliente genérico
+        if ($id === 1) {
+            return response()->json(['message' => 'Este cliente es genérico y no puede eliminarse.'], 403);
         }
+
+        // Verifica si el cliente existe
+        if ($client) {
+            // Eliminar el cliente
+            $client->delete();
+            return response()->json(['message' => 'Cliente eliminado con éxito.', 'client' => $client], 200);
+        } else {
+            return response()->json(['message' => 'Cliente no encontrado.'], 404);
+        }
+    } catch (\Exception $e) {
+        return response()->json(['message' => 'Error al eliminar el cliente.', 'error' => $e->getMessage()], 500);
     }
+}
+
 
     public function getClients(){
         $clients = Client::all();
